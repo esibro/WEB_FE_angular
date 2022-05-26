3246f4d275c00b9ee822e33a94b95fdd204e3434
Esma Gürsoy <esmaguersoy@gmail.com>
2022-05-26 10:57:46 +0200

    list & detail view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,23 @@ import {Course} from "./shared/course";
 
 @Component({
   selector: 'bs-root',
-  template: `<bs-course-list *ngIf="listOn"></bs-course-list>
-  <bs-course-details *ngIf="detailsOn" [course]="course"></bs-course-details>`,
+  template: `<bs-course-list (showDetailsEvent)="showDetails($event)" *ngIf="listOn"></bs-course-list>
+  <bs-course-details (showListEvent)="showList()" *ngIf="detailsOn" [course]="course"></bs-course-details>`,
   styles: []
 })
 export class AppComponent {
   listOn = true;
   detailsOn = false;
+
+  showList() {
+    this.listOn = true;
+    this.detailsOn = false;
+  }
+  showDetails(course: Course) {
+    this.course = course;
+    this.listOn = false;
+    this.detailsOn = true;
+  }
 
   course: Course | undefined;
 }
